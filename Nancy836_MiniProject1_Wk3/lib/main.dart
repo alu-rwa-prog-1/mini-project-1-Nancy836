16cc6d77cd4123a651d40d35280e21630baa7438
Nancy836 <55183861+Nancy836@users.noreply.github.com>
2021-02-11 20:59:10 +0200

    Add files via upload

---
+++ Nancy836_MiniProject1_Wk3/lib/main.dart
@@ -0,0 +1,49 @@
+//importing necessary packages
+import 'package:flutter/material.dart';
+
+//main function
+void main() => runApp(Home());
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //defining brightness and colors
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.yellow,
+        //accentColor:Color.fromRGBO(226, 32, 77, 0.5),
+
+        //defining font family
+        fontFamily: 'Lato',
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fusion'),
+        ),
+        body: Container(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Account',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
